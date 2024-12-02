156ba27f07ebeb5a0fcfe909829f7e204a3378a5
Konrad Konik <88903994+KonradKinok@users.noreply.github.com>
2024-12-02 16:56:08 +0100

    02.12.2024

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,18 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   base: "/goit-react-hw-07-phonebook/",
+//   build: {
+//     outDir: "./dist",
+//   },
+// });
+
+// <https://vitejs.dev/config/>
 export default defineConfig({
   plugins: [react()],
-  base: "/goit-react-hw-07-phonebook/",
   build: {
-    outDir: "./dist",
+    sourcemap: true,
   },
 });
